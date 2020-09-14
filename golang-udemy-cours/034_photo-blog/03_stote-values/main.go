574f2ce44d6d5234b81eeda0751747b6ab8965c0
Ilya Izmailov <mr.izmailovilya@gmail.com>
2020-09-14 23:13:10 +0300

    Completed 034-03

---
+++ golang-udemy-cours/034_photo-blog/03_stote-values/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+	"text/template"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/favicom.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	c := getCookie(w, req)
+	c = appendValue(w, c)
+	cps := strings.Split(c.Value, "|")
+	tpl.ExecuteTemplate(w, "index.gohtml", cps)
+}
+
+func getCookie(w http.ResponseWriter, req *http.Request) *http.Cookie {
+	c, err := req.Cookie("seesion")
+	if err != nil {
+		sID, _ := uuid.NewV4()
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+	}
+	return c
+}
+
+func appendValue(w http.ResponseWriter, c *http.Cookie) *http.Cookie {
+	p1 := "me.png"
+	p2 := "disney.png"
+	p3 := "lida.png"
+	s := c.Value
+	if !strings.Contains(s, p1) {
+		s += "|" + p1
+	}
+	if !strings.Contains(s, p2) {
+		s += "|" + p2
+	}
+	if !strings.Contains(s, p3) {
+		s += "|" + p3
+	}
+	c.Value = s
+	http.SetCookie(w, c)
+	return c
+}
