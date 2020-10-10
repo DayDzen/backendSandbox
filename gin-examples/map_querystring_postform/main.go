3b8ad10638f8f47fa652a2b7efa3260c08c4cdbd
Ilya Izmailov <mr.izmailovilya@gmail.com>
2020-10-10 19:18:32 +0300

    Created and implemendted Map as querystring or postform parameters

---
+++ gin-examples/map_querystring_postform/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	// POST /post?ids[a]=1234&ids[b]=hello HTTP/1.1
+	// Content-Type: application/x-www-form-urlencoded
+	// names[first]=thinkerou&names[second]=tianou
+	router.POST("/post", func(c *gin.Context) {
+		ids := c.QueryMap("ids")
+		names := c.PostFormMap("names")
+		fmt.Printf("ids: %v; names: %v\n", ids, names)
+	})
+	router.Run(":8080")
+}
