59cb91e1a64b4bfd3f0f08a1449459555048172d
Ilya Izmailov <mr.izmailovilya@gmail.com>
2020-10-10 19:08:15 +0300

    query + post form

---
+++ gin-examples/query_post_form/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	// POST /post?id=1234&page=1 HTTP/1.1
+	// Content-Type: application/x-www-form-urlencoded
+	// name=manu&message=this_is_great
+	router.POST("/post", func(c *gin.Context) {
+
+		id := c.Query("id")
+		page := c.DefaultQuery("page", "0")
+		name := c.PostForm("name")
+		message := c.PostForm("message")
+
+		c.String(http.StatusOK, fmt.Sprintf("id: %s; page: %s; name: %s; message: %s", id, page, name, message))
+	})
+	router.Run(":8080")
+}
